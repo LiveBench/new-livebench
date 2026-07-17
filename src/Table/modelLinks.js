ab4d411576be8d6e31a3a61aea36bfbbe624b44e
Arvind Sundararajan <arvind@abacus.ai>
2026-07-17 01:09:56 +0000

    New results

--- src/Table/modelLinks.js
+++ src/Table/modelLinks.js
@@ -274,6 +274,7 @@ export const modelLinks = {
     "kimi-k2-thinking": {url: "https://huggingface.co/moonshotai/Kimi-K2-Thinking", organization: "Moonshot AI", displayName: "Kimi K2 Thinking", version: "2025-11-08", openweight: true, reasoner: true},
     "kimi-k2.5-thinking": {url: "https://www.kimi.com/blog/kimi-k2-5.html", organization: "Moonshot AI", displayName: "Kimi K2.5 Thinking",  openweight: true, reasoner: true},
     "kimi-k2.6-thinking": {url: "https://www.kimi.com/blog/kimi-k2-6", organization: "Moonshot AI", displayName: "Kimi K2.6 Thinking",  openweight: true, reasoner: true},
+    "kimi-k-3": {url: "https://www.kimi.com/blog/kimi-k3", organization: "Moonshot AI", displayName: "Kimi K3",  openweight: true, reasoner: true},
     "grok-4-fast-non-reasoning-2511": {url: "https://docs.x.ai/docs/models/grok-4-fast-non-reasoning", organization: "xAI", displayName: "Grok 4 Fast (Non-Reasoning)", reasoner: false, version: "2025-11-10"},
     "grok-4-fast-reasoning-2511": {url: "https://docs.x.ai/docs/models/grok-4-fast-reasoning", organization: "xAI", displayName: "Grok 4 Fast", reasoner: true, version: "2025-11-10"},
     "gpt-5.1-2025-11-13": {
